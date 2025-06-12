0c36c0de558e83ff1060c691594c4344dc86778f
LIMSEONU <144203174+LIMSEONU@users.noreply.github.com>
2025-06-12 23:15:47 +0900

    Add files via upload

---
+++ 0411/RandomBackground/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.layoutbackgroudex;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ConstraintLayout mainLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        mainLayout = findViewById(R.id.main);
+        Button btnChange = findViewById(R.id.button);
+
+        btnChange.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Random random = new Random();
+                int color = Color.rgb(random.nextInt(256), random.nextInt(256), random.nextInt(256));
+                mainLayout.setBackgroundColor(color);
+            }
+        });
+
+    }
+}
